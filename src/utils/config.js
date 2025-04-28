df1c35ea4516c3e0ed73756aaa44df9a70012ac0
Nikcet <mstislav.kr@yandex.ru>
2025-04-29 00:47:59 +0300

    fix bug

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,5 +1,5 @@
 const PRODUCTION = false;
-const API_URL = PRODUCTION ? "https://wishesbook.ru/api/" : 'http://localhost:8000';
+const API_URL = PRODUCTION ? "https://wishesbook.ru/api" : 'http://localhost:8000';
 
 
 export {API_URL}
