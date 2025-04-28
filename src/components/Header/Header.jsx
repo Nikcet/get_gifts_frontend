4f6f0640029415c11dad74947f69a6017f061f47
Nikcet <mstislav.kr@yandex.ru>
2025-04-28 23:51:04 +0300

    add share button

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,41 +1,69 @@
-import React from 'react';
-import "./Header.css";
-import { AppBar, Toolbar, Typography, Button, Link } from '@mui/material';
-
+import { useContext } from 'react';
+import { useLocation } from 'react-router-dom';
+import {
+    AppBar,
+    Toolbar,
+    Typography,
+    Button,
+    IconButton,
+    Tooltip
+} from '@mui/material';
+import { NotificationContext } from '../../contexts/NotificationContext';
+import ShareIcon from '@mui/icons-material/Share';
 
 
 const Header = ({ isAuthenticated, onLogout, onRegister, onLogin }) => {
+
+    const showNotification = useContext(NotificationContext);
+    const location = useLocation();
+
+    const handleShare = () => {
+        const url = `${window.location.origin}${location.pathname}`;
+        navigator.clipboard.writeText(url)
+            .then(() => {
+                showNotification('Ссылка скопирована в буфер обмена', 'success');
+            })
+            .catch(() => {
+                showNotification('Не удалось скопировать ссылку', 'error');
+            });
+    };
+
     return (
         <AppBar position="static" color="primary" elevation={0}>
             <Toolbar>
                 <Typography variant="h6" component="div" sx={{ flexGrow: 1, fontWeight: 700 }}>
                     Whishes Book
                 </Typography>
-                {!isAuthenticated ? (
+
+                {isAuthenticated ? (
                     <>
+                        <Tooltip title="Поделиться">
+                            <IconButton
+                                color="inherit"
+                                onClick={handleShare}
+                                sx={{ mr: 1 }}
+                                aria-label="Поделиться"
+                            >
+                                <ShareIcon />
+                            </IconButton>
+                        </Tooltip>
+
                         <Button
                             color="inherit"
-                            onClick={onLogin}
-                            sx={{ mr: 1 }}
+                            onClick={onLogout}
                         >
-                            Войти
-                        </Button>
-                        <Button
-                            variant="outlined"
-                            color="inherit"
-                            onClick={onRegister}
-                        >
-                            Регистрация
+                            Выйти
                         </Button>
                     </>
                 ) : (
-                    <Button
-                        variant="outlined"
-                        color="inherit"
-                        onClick={onLogout}
-                    >
-                        Выйти
-                    </Button>
+                    <>
+                        <Button color="inherit" onClick={onLogin} sx={{ mr: 1 }}>
+                            Войти
+                        </Button>
+                        <Button variant="outlined" color="inherit" onClick={onRegister}>
+                            Регистрация
+                        </Button>
+                    </>
                 )}
             </Toolbar>
         </AppBar>
